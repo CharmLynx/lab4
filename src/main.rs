95de6713a0328c0b3dd19bb6bf2f1cd89a3c027a
Maryna <161884856+CharmLynx@users.noreply.github.com>
2024-05-29 20:25:12 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
-fn take_by_reference(_: &i32) {}
+fn take_by_reference(x: &mut i32) {
+    *x += 1;
+}
 
-fn take_by_value(_: i32) {}
+fn take_by_value(x: i32) -> i32 {
+    x + 1
+}
 
 fn main() {
-    let x = 42;
-    take_by_reference(&x);
-    take_by_value(x);
+    let mut x = 42;
+    take_by_reference(&mut x);
+    let y = take_by_value(x);
+    println!("x: {}, y: {}", x, y);
 }
