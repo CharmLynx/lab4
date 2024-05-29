527f190563a0dc28c5a5d23ad0f1cf5e850ad20a
Maryna <161884856+CharmLynx@users.noreply.github.com>
2024-05-29 20:26:47 +0300

    Update ReferenceTypes_test.rs

--- tests/ReferenceTypes_test.rs
+++ tests/ReferenceTypes_test.rs
@@ -1,20 +1,11 @@
-fn take_by_reference(_: &i32) {}
-
-fn main() {
-    let x = 42;
-    take_by_reference(&x);
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_reference_is_passed() {
-        let x = 42;
-        let x_ptr_before = &x as *const i32;
-        take_by_reference(&x);
-        let x_ptr_after = &x as *const i32;
-        assert_eq!(x_ptr_before, x_ptr_after);
+    fn test_reference_is_modified() {
+        let mut x = 42;
+        take_by_reference(&mut x);
+        assert_eq!(x, 43);
     }
 }
