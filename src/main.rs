3e964fc8e085d09cb14494fcca816fb52da8762f
Maryna <161884856+CharmLynx@users.noreply.github.com>
2024-06-01 09:44:03 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,17 @@ fn take_by_reference(x: &mut i32) {
     *x += 1;
 }
 
-fn take_by_value(y: i32) -> i32 {
+fn take_by_value(y: i32) -> i32 { //не спрацює, оскільки по замовчуванню y - immutable argument
     y += 1;
     y
 }
 fn main() {
     let mut x = 42;
     take_by_reference(&mut x);
-    let y = 42;
-    let y = take_by_value(y);
+    let x = 42;
+    let x = match take_by_value(x) {
+       value if value >= 0 => value,
+       _ => x
+    };
     println!("x: {}, y: {}", x, y);
 }
