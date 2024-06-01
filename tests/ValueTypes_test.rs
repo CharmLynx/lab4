cc0b042b28554829cf4bc63953fd03a3441ddfa2
Maryna <161884856+CharmLynx@users.noreply.github.com>
2024-06-01 09:14:50 +0300

    Update ValueTypes_test.rs

--- tests/ValueTypes_test.rs
+++ tests/ValueTypes_test.rs
@@ -16,7 +16,7 @@ mod tests {
     #[test]
     fn test_value_is_copied() {
         let x = 42;
-        let y = take_by_value(x);
+        let x = take_by_value(x);
         assert_eq!(x, 42);
         //assert_eq!(y, 43);
     }
