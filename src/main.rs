ee06d2b0705b83c8143fc192d4ff2b09b766c855
Maryna <161884856+CharmLynx@users.noreply.github.com>
2024-06-01 09:06:08 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,10 @@ fn take_by_reference(x: &mut i32) {
     *x += 1;
 }
 
-fn take_by_value(x: i32) -> i32 {
-    x += 1
+fn take_by_value(mut x: i32) -> i32 {
+    x += 1;
     x
 }
-
 fn main() {
     let mut x = 42;
     take_by_reference(&mut x);
